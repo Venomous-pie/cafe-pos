36163c4926e42874e9fdaa0f529158d3a2ada3f5
Venomous-pie <dedicatoria.work@gmail.com>
2026-07-14 15:42:27 +0800

    update: category card size update

--- app/components/OrderPanel.tsx
+++ app/components/OrderPanel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { JSX, useState } from "react";
 import menuData from "@/data/menu.json";
 
 interface CartItem {
